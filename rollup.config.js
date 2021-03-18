6379e49631bd47a918917f4159fbeb51e49c020c
Ali Fathieh <Ali.Fathieh@latitudefinancial.com>
2021-03-18 17:02:01 +1100

    playground removed.

--- rollup.config.js
+++ rollup.config.js
@@ -7,11 +7,6 @@ export default [
     input: "src/index.ts",
     output: [
       {
-        file: "playground/src/component-lib/index.js",
-        format: "esm",
-        banner: "/* eslint-disable */",
-      },
-      {
         file: "website/lib/basis-lib/index.js",
         format: "esm",
         banner: "/* eslint-disable */",
@@ -20,7 +15,7 @@ export default [
       { file: pkg.module, format: "esm" },
     ],
     plugins: [
-      del({ targets: ["dist/*", "playground/src/component-lib"] }),
+      del({ targets: ["dist/*", "website/lib/basis-lib"] }),
       typescript({
         include: ["src/*.js", "src/**/*.js", "src/*.ts(x)", "src/**/*.ts(x)"],
       }),

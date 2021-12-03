fbc64cb0f0cf13575d1b13180f387cc06d1ad8f2
Ali Fathieh <12068482+alifa20@users.noreply.github.com>
2021-12-03 13:59:13 +1100

    fix: dropdown added

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Input } from './Input';
 export { default as Select } from './Select';
 export { default as DateInput } from './DateInput';
+export { default as Dropdown } from './Dropdown/Dropdown';
 export { default as Button } from './Button';
 export { default as Modal } from './Modal';
 
